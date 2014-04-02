6d96bd355838e4eea5e41e83ae762ca32455eead
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-02 15:30:32 +0200

    added hash.php

---
+++ hash.php
@@ -0,0 +1,19 @@
+<form name="hash" id="hash" action="hash.php" method="post" >
+
+<div id="hash">
+	<p>To be hashed:</p>
+	<input type="text" name="hash" value="" >
+</div>
+
+<div id="submit">
+	<input type="submit" value="generate hash">
+</div>
+
+</form>
+
+<?php
+$hash = $_POST["hash"];
+echo(password_hash($hash));
+
+
+?>
