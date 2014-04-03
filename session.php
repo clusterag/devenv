49ea435b527dbcbd1916f6bfa3836f894c093135
John Eberle <john@clusterag.leiningergymnasium.de>
2014-04-03 19:27:05 +0200

    session

--- session.php
+++ session.php
@@ -1,7 +1,24 @@
+<form name="session" id="session" action="session.php" method="post" >
+
+<div id="user">
+	<p>User:</p>
+	<input type="text" name="user" value="" >
+</div>
+
+<div id="submit">
+	<input type="submit" value="submit">
+</div>
+
+</form>
 <?php
 
 session_start();
-echo($_SESSION["user"]);
-$_SESSION["user"] = "test";
+if (isset($_SESSION["user"])){
+	echo($_SESSION["user"]);
+}
+else {
+	$_SESSION["user"] = $_POST["user"];
+}
+
 
 ?>
